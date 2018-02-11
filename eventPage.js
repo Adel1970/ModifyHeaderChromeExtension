9284aa7f5c61e8cc2364cfd23050b0fe6ab96971
adel <extensa1956@gmail.com>
2018-02-11 23:07:46 +0100

    hardcoded modified header

--- eventPage.js
+++ eventPage.js
@@ -1,8 +1,22 @@
-chrome.webRequest.onBeforeRequest.addListener(
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
     function(details) {
         console.log("Request URL:" + details.url)
-        return {cancel: true}; 
+        for(var i = 0; i < details.requestHeaders.length; ++i){
+            if(details.requestHeaders[i].name === 'User-Agent'){
+                console.log(details.requestHeaders[i]);
+                console.log('details.requestHeaders.length'+ details.requestHeaders.length);
+                // details.requestHeaders.splice(i, 1, {'User-Agent': 'ranorex'});
+                details.requestHeaders[i].value = 'ranorex';
+                console.log(details.requestHeaders[i]);
+                console.log('details.requestHeaders.length'+ details.requestHeaders.length);
+                break;
+            }
+        }
+        console.log('details.requestHeaders'+ details.requestHeaders.toString());
+        console.log('details.requestHeaders.length'+ details.requestHeaders.length);
+        return {requestHeaders: details.requestHeaders}
     },
-    {urls: ["https://www.hays.de/"]},
-    ["blocking"]);
+        {urls: ["https://www.hays.de/"]},
+        ["blocking", "requestHeaders"]);
 
--- popup.js
+++ popup.js
@@ -1,2 +1,16 @@
-$(function(){
+// // $(function () {
+// //     chrome.storage.sync.get(['name', 'value'], function(items) {
+// //         $('#name').html(items.name); 
+// //         $('#value').html(items.value); 
+// //     }); 
+//     $('#addHeader').click(function () {
+//         // chrome.storage.sync.get(['name', 'value'], function(items) {
+//         var name = $('#addName').val(); 
+//         var value = $('#addValue').val(); 
+//         chrome.storage.sync.set({'name':name});
+//         chrome.storage.sync.set({'value':value});
+//                 $('#name').text(name); 
+//                 $('#value').text(value); 
+//             })
+//     }); 
 
