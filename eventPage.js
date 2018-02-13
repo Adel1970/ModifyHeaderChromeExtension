af426c2e1e764c2ba8a9c82e6aadce9557f7bdfb
adel <extensa1956@gmail.com>
2018-02-13 14:11:15 +0100

    still working

--- eventPage.js
+++ eventPage.js
@@ -1,22 +1,23 @@
-
+$(function () {
+    console.log('headerValue: ' + $('#value').val()); 
 chrome.webRequest.onBeforeSendHeaders.addListener(
-    function(details) {
-        console.log("Request URL:" + details.url)
-        for(var i = 0; i < details.requestHeaders.length; ++i){
-            if(details.requestHeaders[i].name === 'User-Agent'){
-                console.log(details.requestHeaders[i]);
-                console.log('details.requestHeaders.length'+ details.requestHeaders.length);
+    function (details) {
+        // console.log("Request URL:" + details.url)
+        for (var i = 0; i < details.requestHeaders.length;  ++i) {
+            if (details.requestHeaders[i].name === 'User-Agent') {
+                // console.log(details.requestHeaders[i]);
+                // console.log('details.requestHeaders.length'+ details.requestHeaders.length);
                 // details.requestHeaders.splice(i, 1, {'User-Agent': 'ranorex'});
-                details.requestHeaders[i].value = 'ranorex';
-                console.log(details.requestHeaders[i]);
-                console.log('details.requestHeaders.length'+ details.requestHeaders.length);
-                break;
+                details.requestHeaders[i].value =  'ranorex'; 
+                // console.log(details.requestHeaders[i]);
+                // console.log('details.requestHeaders.length'+ details.requestHeaders.length);
+                break; 
             }
         }
-        console.log('details.requestHeaders'+ details.requestHeaders.toString());
-        console.log('details.requestHeaders.length'+ details.requestHeaders.length);
-        return {requestHeaders: details.requestHeaders}
-    },
-        {urls: ["https://www.hays.de/"]},
-        ["blocking", "requestHeaders"]);
-
+        // console.log('details.requestHeaders'+ details.requestHeaders.toString());
+        // console.log('details.requestHeaders.length'+ details.requestHeaders.length);
+        return {requestHeaders:details.requestHeaders}
+    },  {urls:["https://www.hays.de/"]},
+        ["blocking", "requestHeaders"]); 
+    
+}); 
