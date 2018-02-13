f1e753596fb37bb229feea1d2ccc7cef06c964a6
adel <extensa1956@gmail.com>
2018-02-13 19:51:45 +0100

    fixed the unchanged header (after first loading of the extension) problem using messaging between popup.js & eventPage.js(background)

--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,7 @@
-// chrome.runtime.sendMessage({
-//     headerName: "blablabla"
+// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+
+
+// // chrome.runtime.sendMessage({
+// //     headerName: "blablabla"
+// // })
 // })
--- eventPage.js
+++ eventPage.js
@@ -1,24 +1,29 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-        var headerName = request.headerName;
-    console.log('headerName: ' + request.headerName, ' headerValue: ' + request.headerValue);
-     
-chrome.webRequest.onBeforeSendHeaders.addListener(
-    function (details) {
-        // console.log("Request URL:" + details.url)
-        for (var i = 0; i < details.requestHeaders.length;  ++i) {
-            if (details.requestHeaders[i].name === request.headerName) {
-                // console.log(details.requestHeaders[i]);
-                // console.log('details.requestHeaders.length'+ details.requestHeaders.length);
-                // details.requestHeaders.splice(i, 1, {'User-Agent': 'ranorex'});
-                details.requestHeaders[i].value =  request.headerValue; 
-                // console.log(details.requestHeaders[i]);
-                // console.log('details.requestHeaders.length'+ details.requestHeaders.length);
-                break; 
+$(function () {
+    var headerName, headerValue; 
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    headerName = request.headerName; 
+    headerValue = request.headerValue; 
+    console.log('headerName: ' + request.headerName, ' headerValue: ' + request.headerValue); 
+}); 
+console.log('headerName: ' + headerName, ' headerValue: ' + headerValue); 
+ chrome.webRequest.onBeforeSendHeaders.addListener(
+        function (details) {
+            for (var i = 0; i < details.requestHeaders.length; i++) {
+                    console.log('details.requestHeaders[' + i + '].name: ' + details.requestHeaders[i].name, 'details.requestHeaders[' + i + '].value: ' + details.requestHeaders[i].value)
+                if (details.requestHeaders[i].name === headerName) {
+                    // console.log(details.requestHeaders[i]);
+                    // console.log('details.requestHeaders.length'+ details.requestHeaders.length);
+                    // details.requestHeaders.splice(i, 1, {'User-Agent': 'ranorex'});
+                    // console.log("request.headerValue Before: ", request.headerValue);
+                    details.requestHeaders[i].value = headerValue; 
+                    console.log('details.requestHeaders[' + i + '].value inside if statement: ' + details.requestHeaders[i].value)
+                    // console.log("request.headerValue after: ", request.headerValue);
+                    // console.log(details.requestHeaders[i]);
+                    // console.log('details.requestHeaders.length'+ details.requestHeaders.length);
+                    break; 
+                }
             }
-        }
-        // console.log('details.requestHeaders'+ details.requestHeaders.toString());
-        // console.log('details.requestHeaders.length'+ details.requestHeaders.length);
         return {requestHeaders:details.requestHeaders}
     },  {urls:["https://www.hays.de/"]},
         ["blocking", "requestHeaders"]); 
-    });
+    }); 
