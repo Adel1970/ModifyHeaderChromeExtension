26ff6c523ba57899f6ec9a39bc78d386481bf25f
adel <extensa1956@gmail.com>
2018-02-14 23:34:41 +0100

    implemented adding multiple headers to the headerTable but enabling only one heeader

--- popup.js
+++ popup.js
@@ -1,22 +1,51 @@
 $(function () {
-    chrome.storage.sync.get(['name', 'value'], function(items) {
-        $('#name').html(items.name); 
-        $('#value').html(items.value); 
+    chrome.storage.sync.get( {headers:[]}, function(result) {
+        var headers = result.headers; 
+        console.log('row headers array', headers); 
+        for (var i = 0; i < headers.length; i++) {
+            console.log('headers[' + i + ']:', headers[i]); 
+            console.log('headers[' + i + '].name: ', headers[i].name, 'headers[' + i + '].value: ', headers[i].value)
+            $('#headerTable tr:last').after(
+                '<tr>' + 
+                '<td class="nameClass" id="name' + i + '">' + headers[i].name + '</td>' + 
+                '<td class="valueClass" id="value' + i + '">' + headers[i].value + '</td>' + 
+                '<td ><button class="enable" id="enable' + i + '" type="button">enable</button></td>' + 
+                '</tr>'); 
+                console.log($('.enable').attr('id'))
+        }
     }); 
-  
     $('#addHeader').click(function () {
-        // chrome.storage.sync.get(['name', 'value'], function(items) {
         var name = $('#addName').val(); 
         var value = $('#addValue').val(); 
-        chrome.storage.sync.set( {'name':name}); 
-        chrome.storage.sync.set( {'value':value}); 
-                $('#name').text(name); 
-                $('#value').text(value); 
-            })
-    $('#enable').click(function () {
-        chrome.runtime.sendMessage({
-                headerName: $('#name').html(),
-                headerValue: $('#value').html()
+        $('#headerTable tr:last').after(
+            '<tr>' + 
+            '<td class="nameClass">' + name + '</td>' + 
+            '<td class="valueClass">' + value + '</td>' + 
+            '<td><button class="enable" type="submit">enable</button></td>' + 
+            '</tr>'); 
+        chrome.storage.sync.get( {headers:[]}, function(result) {
+        var headers = result.headers; 
+        var header =  {
+            'name':name, 
+            'value':value
+        }
+        headers.push(header); 
+        chrome.storage.sync.set( {headers:headers}, function() {
+            chrome.storage.sync.get( {headers:[]}, function(result) {
+                console.log('headers array after calling set: ' + result.headers); 
+            }); 
+        }); 
+    }); 
+}); 
+    
+    $(document).on('click', '#headerTable tbody tr td button.enable', function(event){
+        console.log('click function');
+        var headerName = $(this).closest('tr').find('.nameClass').html(); 
+        var headerValue = $(this).closest('tr').find('.valueClass').html(); 
+        console.log('click function headerName: ', headerName, '\n', 'click function headerValue: ', headerValue); 
+        chrome.runtime.sendMessage( {
+                headerName: headerName,
+                headerValue: headerValue
         }); 
     }); 
 }); 
