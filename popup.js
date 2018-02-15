4fbd59933a0f728624dc0fc69bf87c9b8ce1bb58
adel <extensa1956@gmail.com>
2018-02-15 13:27:50 +0100

    old way

--- popup.js
+++ popup.js
@@ -4,12 +4,13 @@ $(function () {
         console.log('row headers array', headers); 
         for (var i = 0; i < headers.length; i++) {
             console.log('headers[' + i + ']:', headers[i]); 
-            console.log('headers[' + i + '].name: ', headers[i].name, 'headers[' + i + '].value: ', headers[i].value)
+            console.log('headers[' + i + '].name: ', headers[i].name, ' headers[' + i + '].value: ', headers[i].value)
             $('#headerTable tr:last').after(
                 '<tr>' + 
                 '<td class="nameClass" id="name' + i + '">' + headers[i].name + '</td>' + 
                 '<td class="valueClass" id="value' + i + '">' + headers[i].value + '</td>' + 
-                '<td ><button class="enable" id="enable' + i + '" type="button">enable</button></td>' + 
+                '<td><button class="enable" id="enable' + i + '" type="button">enable</button></td>' + 
+                '<td><button class="remove" id="remove'+i+'" type="button">remove</button></td>' +
                 '</tr>'); 
                 console.log($('.enable').attr('id'))
         }
@@ -22,8 +23,9 @@ $(function () {
             '<td class="nameClass">' + name + '</td>' + 
             '<td class="valueClass">' + value + '</td>' + 
             '<td><button class="enable" type="submit">enable</button></td>' + 
+            '<td><button class="remove" type="button">remove</button></td>' +
             '</tr>'); 
-        chrome.storage.sync.get( {headers:[]}, function(result) {
+        chrome.storage.sync.get({headers:[]}, function(result) {
         var headers = result.headers; 
         var header =  {
             'name':name, 
@@ -31,9 +33,11 @@ $(function () {
         }
         headers.push(header); 
         chrome.storage.sync.set( {headers:headers}, function() {
-            chrome.storage.sync.get( {headers:[]}, function(result) {
-                console.log('headers array after calling set: ' + result.headers); 
-            }); 
+            chrome.storage.sync.get( {headers:[]}, function(result) {//just for testing, must be omitted
+                for (var i = 0; i < headers.length; i++) {
+                    console.log('headers[' + i + ']:', headers[i]); 
+                    console.log('headers[' + i + '].name: ', headers[i].name, ' headers[' + i + '].value: ', headers[i].value) //just for testing, must be omitted
+                }}); 
         }); 
     }); 
 }); 
@@ -48,4 +52,34 @@ $(function () {
                 headerValue: headerValue
         }); 
     }); 
+    $(document).on('click', '#headerTable tbody tr td button.remove', function(event){
+        console.log('remove function called');
+        var buttonId = event.target.id;
+        console.log('buttonId: ', buttonId);
+        var IndexOfButtonNumericOrder = event.target.id.lastIndexOf('e') +1;
+        console.log('buttonNumericOrderIndex:', IndexOfButtonNumericOrder);
+        var buttonNumericOrder = event.target.id.substring(IndexOfButtonNumericOrder, buttonId.length);
+             $(this).closest('tr').remove();
+        console.log('buttonNumericOrder: ',buttonNumericOrder);
+        chrome.storage.sync.get({headers:[]}, function(result){
+            var headers = result.headers;
+            for (var i = 0; i < headers.length; i++) {
+                console.log('headers[' + i + '].name: ', headers[i].name, ' headers[' + i + '].value: ', headers[i].value)
+            }
+            // if()
+            // buttonNumericOrder = --buttonNumericOrder;
+            // console.log('ButtonNumericOrder after decrementing: ',buttonNumericOrder)
+            headers.splice(buttonNumericOrder, 1);
+            // console.log('headers[' + buttonNumericOrder + '].name: ', headers[buttonNumericOrder].name, 'headers[' + buttonNumericOrder + '].value: ', headers[buttonNumericOrder].value)
+            chrome.storage.sync.set({headers: headers}, function(){
+                chrome.storage.sync.get({headers:[]}, function(result){//just for testing, must be omitted
+                    for (var i = 0; i < headers.length; i++) {
+                        console.log('headers[' + i + '].name: ', headers[i].name, 'headers[' + i + '].value: ', headers[i].value)
+                    }
+                    console.log('headers array after removing elements: ', result.headers.length)//just for testing, must be omitted
+                });
+            });
+        });
+   
+    });
 }); 
