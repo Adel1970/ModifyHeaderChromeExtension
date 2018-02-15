9fe4e4b85d1e82f5153499ea5d79eb373401c3af
adel <extensa1956@gmail.com>
2018-02-15 22:59:29 +0100

    initial commit

--- eventPage.js
+++ eventPage.js
@@ -1,29 +1,35 @@
 $(function () {
-    var headerName, headerValue; 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    headerName = request.headerName; 
-    headerValue = request.headerValue; 
-    console.log('headerName: ' + request.headerName, ' headerValue: ' + request.headerValue); 
-}); 
-console.log('headerName: ' + headerName, ' headerValue: ' + headerValue); 
- chrome.webRequest.onBeforeSendHeaders.addListener(
+    var enabledHeaders = [];
+    chrome.storage.sync.get({enabledHeaders:[]},function(result){
+        enabledHeaders = result.enabledHeaders;
+        for(var i = 0; i<enabledHeaders.length; i++){
+            console.log('headerName['+i+']: ' + enabledHeaders[i].name, ' headerValue['+i+']: ' + enabledHeaders[i].value); 
+        }
+    });
+    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+        enabledHeaders = enabledHeaders.push(request.enabledHeaders);
+        for(var i = 0; i<enabledHeaders.length; i++){
+        console.log('headerName['+i+']: ' + enabledHeaders[i].name, ' headerValue['+i+']: ' + enabledHeaders[i].value); 
+        }
+    }); 
+    chrome.webRequest.onBeforeSendHeaders.addListener(
         function (details) {
-            for (var i = 0; i < details.requestHeaders.length; i++) {
-                    console.log('details.requestHeaders[' + i + '].name: ' + details.requestHeaders[i].name, 'details.requestHeaders[' + i + '].value: ' + details.requestHeaders[i].value)
-                if (details.requestHeaders[i].name === headerName) {
-                    // console.log(details.requestHeaders[i]);
-                    // console.log('details.requestHeaders.length'+ details.requestHeaders.length);
-                    // details.requestHeaders.splice(i, 1, {'User-Agent': 'ranorex'});
-                    // console.log("request.headerValue Before: ", request.headerValue);
-                    details.requestHeaders[i].value = headerValue; 
-                    console.log('details.requestHeaders[' + i + '].value inside if statement: ' + details.requestHeaders[i].value)
-                    // console.log("request.headerValue after: ", request.headerValue);
-                    // console.log(details.requestHeaders[i]);
-                    // console.log('details.requestHeaders.length'+ details.requestHeaders.length);
-                    break; 
+            for(var i = 0; i<enabledHeaders.length; i++){
+                for (var j = 0; j < details.requestHeaders.length; j++) {
+                    console.log('details.requestHeaders[' + j + '].name: ' + details.requestHeaders[j].name, 
+                                'details.requestHeaders[' + j + '].value: ' + details.requestHeaders[j].value)
+                    if (details.requestHeaders[j].name === enabledHeaders[i].name) {
+                        details.requestHeaders[j].value = enabledHeaders[i].value; 
+                        console.log('details.requestHeaders[' + j + '].value inside if statement: ' + details.requestHeaders[j].value)
+                        break; 
+                    }else{
+                        details.requestHeaders.push(enabledHeaders[i]);
+                        break;
+                    }
                 }
             }
-        return {requestHeaders:details.requestHeaders}
-    },  {urls:["https://www.hays.de/"]},
-        ["blocking", "requestHeaders"]); 
-    }); 
+    return {requestHeaders:details.requestHeaders}
+    },  
+    {urls:["https://www.hays.de/"]},
+    ["blocking", "requestHeaders"]); 
+}); 
