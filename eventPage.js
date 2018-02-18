73f45575c96c743246f25fe225370897d0202e64
adel <extensa1956@gmail.com>
2018-02-18 18:49:15 +0100

    reset the names of the loops variables in eventPage to i and j

--- eventPage.js
+++ eventPage.js
@@ -15,43 +15,43 @@ $(function () {
     chrome.webRequest.onBeforeSendHeaders.addListener(
         function (details) {
             enabledHeadersLoop:
-            for(var enabledHeader = 0; enabledHeader < enabledHeaders.length; enabledHeader++) {
+            for(var i = 0; i < enabledHeaders.length; i++) {
                 requestHeadersLoop:
-                for (var requestHeader = 0; requestHeader < details.requestHeaders.length; requestHeader++) {
-                    console.log('round Nr.'+requestHeader+ '\n',
+                for (var j = 0; j < details.requestHeaders.length; j++) {
+                    console.log('round Nr.'+j+ '\n',
                                 'Before processing any condition: ' + '\n',
-                                'details.requestHeaders[' + requestHeader + '].name: ' + details.requestHeaders[requestHeader].name+ '\n', 
-                                'details.requestHeaders[' + requestHeader + '].value: ' + details.requestHeaders[requestHeader].value+ '\n',
+                                'details.requestHeaders[' + j + '].name: ' + details.requestHeaders[j].name+ '\n', 
+                                'details.requestHeaders[' + j + '].value: ' + details.requestHeaders[j].value+ '\n',
                                 'details.requestHeaders.length: ' + details.requestHeaders.length + '\n',
-                                'enabledHeaders['+enabledHeader+'].name: '+ enabledHeaders[enabledHeader].name+ '\n',
-                                'enabledHeaders['+enabledHeader+'].value: ', enabledHeaders[enabledHeader].value);
-                    if (details.requestHeaders[requestHeader].name === enabledHeaders[enabledHeader].name) {
-                        details.requestHeaders[requestHeader].value = enabledHeaders[enabledHeader].value; 
-                        console.log('details.requestHeaders[' + requestHeader + '].value was changed to: ' + details.requestHeaders[requestHeader].value)
+                                'enabledHeaders['+i+'].name: '+ enabledHeaders[i].name+ '\n',
+                                'enabledHeaders['+i+'].value: ', enabledHeaders[i].value);
+                    if (details.requestHeaders[j].name === enabledHeaders[i].name) {
+                        details.requestHeaders[j].value = enabledHeaders[i].value; 
+                        console.log('details.requestHeaders[' + j + '].value was changed to: ' + details.requestHeaders[j].value)
                         break requestHeadersLoop;
                     }
                     else {
-                        while(requestHeader < details.requestHeaders.length) {
-                            if (requestHeader === details.requestHeaders.length -1) {
-                                details.requestHeaders.splice(details.requestHeaders.length, 0, enabledHeaders[enabledHeader]);
-                                console.log('round Nr.'+requestHeader+ '\n',
+                        while(j < details.requestHeaders.length) {
+                            if (j === details.requestHeaders.length -1) {
+                                details.requestHeaders.splice(details.requestHeaders.length, 0, enabledHeaders[i]);
+                                console.log('round Nr.'+j+ '\n',
                                             'RequestHeader reached end of the requestHeaders array ___ ADDING NEW HEADER : ' + '\n',
                                             'details.requestHeaders['+(details.requestHeaders.length-1) +'].name: '+ details.requestHeaders[details.requestHeaders.length-1].name+ '\n', 
                                             'details.requestHeaders['+(details.requestHeaders.length-1) +'].value: ' + details.requestHeaders[details.requestHeaders.length-1].value + '\n',
                                             'details.requestHeaders.length: ' + details.requestHeaders.length+ '\n',
-                                            'enabledHeaders['+enabledHeader+'].name: '+ enabledHeaders[enabledHeader].name+ '\n',
-                                            'enabledHeaders['+enabledHeader+'].value:', enabledHeaders[enabledHeader].value);
+                                            'enabledHeaders['+i+'].name: '+ enabledHeaders[i].name+ '\n',
+                                            'enabledHeaders['+i+'].value:', enabledHeaders[i].value);
                                 // details.requestHeaders = details.requestHeaders.splice(0, details.requestHeaders.length);
                                 console.log( 'details.requestHeaders.length: ' + details.requestHeaders.length);
                                 break requestHeadersLoop;
                             }
                             else {
-                                console.log('round Nr.'+requestHeader+' enabledHeader does not equal requestHeader ___ CONTINUE', '\n',
-                                'details.requestHeaders[' + requestHeader + '].name: ' + details.requestHeaders[requestHeader].name+ '\n', 
-                                'details.requestHeaders[' + requestHeader + '].value: ' + details.requestHeaders[requestHeader].value+ '\n',
+                                console.log('round Nr.'+j+' enabledHeader does not equal requestHeader ___ CONTINUE', '\n',
+                                'details.requestHeaders[' + j + '].name: ' + details.requestHeaders[j].name+ '\n', 
+                                'details.requestHeaders[' + j + '].value: ' + details.requestHeaders[j].value+ '\n',
                                 'details.requestHeaders.length: ' + details.requestHeaders.length + '\n',
-                                'enabledHeaders['+enabledHeader+'].name: '+ enabledHeaders[enabledHeader].name+ '\n',
-                                'enabledHeaders['+enabledHeader+'].value: ', enabledHeaders[enabledHeader].value);                      
+                                'enabledHeaders['+i+'].name: '+ enabledHeaders[i].name+ '\n',
+                                'enabledHeaders['+i+'].value: ', enabledHeaders[i].value);                      
                                 continue requestHeadersLoop;
                             }
                         }
@@ -60,7 +60,6 @@ $(function () {
             }   
             return {requestHeaders:details.requestHeaders}
         },  
-        {urls:["https://www.hays.de/"]},
-        ["blocking", "requestHeaders"]
-    ); 
+        {urls: ["<all_urls>"]},
+        ["blocking", "requestHeaders"]);
 }); 
